4a6b9539d2cdfc4ab505008ac57254e8b8717ca3
Taran677 <154905330+Taran677@users.noreply.github.com>
2024-01-13 17:00:53 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -27,13 +27,15 @@ let a = setInterval(() => {
 
 
   console.log(bulb1)
-  bulb1.onclick = () => {
-
-    bulb1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="rgb(80, 107, 130)" class="bi bi-lightbulb" viewBox="0 0 16 16">
+  if (bulb1 != null) {
+    bulb1.onclick = () => {
+      console.log(bulb1)
+      bulb1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="rgb(80, 107, 130)" class="bi bi-lightbulb" viewBox="0 0 16 16">
                 <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1"/>
               </svg>`
-    bulb1.setAttribute("id", "bulb")
-    document.body.style.background = "url(https://media.istockphoto.com/id/861980226/photo/vintage-style-living-room.jpg?s=612x612&w=0&k=20&c=UUVXNK_TmLAHxzTb-qy6BcuWVcSKPvGTqmBinr_aS4c=)"
-    document.body.innerHTML = document.body.innerHTML + "<style> #body{ backdrop-filter:brightness(20%)} <style/>"
+      bulb1.setAttribute("id", "bulb")
+      document.body.style.background = "url(https://media.istockphoto.com/id/861980226/photo/vintage-style-living-room.jpg?s=612x612&w=0&k=20&c=UUVXNK_TmLAHxzTb-qy6BcuWVcSKPvGTqmBinr_aS4c=)"
+      document.body.innerHTML = document.body.innerHTML + "<style> #body{ backdrop-filter:brightness(20%)} <style/>"
+    }
   }
 }, 500)
